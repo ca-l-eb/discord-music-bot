bed0c7f8843f72dcb4131bb618928d070cf0dc0e
Caleb Dechant <calebdechant@gmail.com>
2017-10-21 00:13:07 -0700

    Update voice_state_listener; threaded voice gateway
    After successfully being notified by VOICE_SERVER_UPDATE,
    voice_state_listener will create a thread to poll events from
    the voice_gateway. On destruction of the listener, notifies the thread
    to stop and joins it. At the moment there is no forceful method to
    make the thread stop if it is blocked on a next_event() call.

--- src/events/voice_state_listener.cc
+++ src/events/voice_state_listener.cc
@@ -1,40 +1,67 @@
+#include <cmd/tls_socket.h>
+#include <iostream>
+
 #include "voice_state_listener.h"
 
-cmd::discord::voice_state_listener::voice_state_listener(cmd::discord::gateway *gateway,
-                                                         const std::string &user_id,
+cmd::discord::voice_state_listener::voice_state_listener(const std::string &user_id,
                                                          const std::string &channel_id,
                                                          const std::string &guild_id)
-    : gateway{gateway}, user_id{user_id}, channel_id{channel_id}, guild_id{guild_id}
+    : user_id{user_id}, channel_id{channel_id}, guild_id{guild_id}
 {
 }
 
-void cmd::discord::voice_state_listener::handle(gtw_op_recv, const nlohmann::json &data,
-                                                const std::string &type)
+cmd::discord::voice_state_listener::~voice_state_listener()
+{
+    alive = false;
+    if (voice_gateway_thread.joinable())
+        voice_gateway_thread.join();
+}
+
+void cmd::discord::voice_state_listener::handle(cmd::discord::gateway &, gtw_op_recv,
+                                                const nlohmann::json &data, const std::string &type)
 {
     if (type == "VOICE_STATE_UPDATE") {
         // We're looking for voice state update for this user_id, in this channel_id, for this
         // guild_id
         auto user = data["user_id"];
-        if (user.is_string() && user != user_id)
+        if (!(user.is_string() && user == user_id))
             return;
 
         auto channel = data["channel_id"];
-        if (channel.is_string() && channel != channel_id)
-            return;
-
-        auto guild = data["guild_id"];
-        if (guild.is_string() && guild != guild_id)
+        if (!(channel.is_string() && channel == channel_id))
             return;
 
         // This must be the session_id we want
-        session_id = data["session_id"];
-    }
-    if (type == "VOICE_SERVER_UPDATE") {
-        std::string token = data["token"];
-        std::string guild_id = data["guild_id"];
-        std::string endpoint = data["endpoint"];
+        session_id = data.at("session_id").get<std::string>();
+    } else if (type == "VOICE_SERVER_UPDATE") {
+        if (session_id.empty())
+            return;
 
-        if (guild_id == this->guild_id && !session_id.empty()) {
+        if (data.at("guild_id").get<std::string>() != guild_id)
+            return;
+
+        std::string token = data.at("token").get<std::string>();
+        std::string endpoint = data.at("endpoint").get<std::string>();
+
+        // We are ready to create the voice_gateway
+        voice_gateway_ptr =
+            std::make_unique<voice_gateway>(endpoint, user_id, session_id, guild_id, token);
+        voice_gateway_thread = std::thread{&voice_state_listener::voice_event_loop, this};
+        alive = true;
+    }
+}
+
+void cmd::discord::voice_state_listener::voice_event_loop()
+{
+    int errors = 0;
+    while (alive) {
+        try {
+            voice_gateway_ptr->next_event();
+        } catch (std::exception &e) {
+            std::cerr << "Voice gateway exception: " << e.what() << "\n";
+            errors++;
+            if (errors == 10)
+                break;
         }
     }
 }
--- src/events/voice_state_listener.h
+++ src/events/voice_state_listener.h
@@ -1,6 +1,8 @@
 #ifndef CMD_DISCORD_VOICE_STATE_LISTENER_H
 #define CMD_DISCORD_VOICE_STATE_LISTENER_H
 
+#include <gateway.h>
+#include <voice_gateway.h>
 #include "event_listener.h"
 
 namespace cmd
@@ -10,13 +12,20 @@ namespace discord
 class voice_state_listener : public event_listener
 {
 public:
-    voice_state_listener(cmd::discord::gateway *gateway, const std::string &user_id,
-                         const std::string &channel_id, const std::string &guild_id);
-    void handle(gtw_op_recv, const nlohmann::json &json, const std::string &type) override;
+    voice_state_listener(const std::string &user_id, const std::string &channel_id,
+                         const std::string &guild_id);
+    ~voice_state_listener();
+    void handle(cmd::discord::gateway &gateway, gtw_op_recv, const nlohmann::json &json,
+                const std::string &type) override;
 
 private:
-    cmd::discord::gateway *gateway;
     std::string user_id, channel_id, guild_id, session_id;
+    std::unique_ptr<cmd::discord::voice_gateway> voice_gateway_ptr;
+
+    std::thread voice_gateway_thread;
+    bool alive;
+
+    void voice_event_loop();
 };
 }
 }
