c7400dec890555a24c6067e379c457aa058882f3
Caleb Dechant <calebdechant@gmail.com>
2017-10-21 00:00:25 -0700

    Update gateway,
    Changed gateway interface for adding event listeners. Event listener now
    subscribe to gateway events and give a name to identify the listener.
    This is to allow for more efficient lookups of handlers when getting
    specific gateway events. The 'ALL' event can be requested, in which case
    the listener is notified of all events.
    
    Start working on support for resuming connections, but this is untested
    and the reference is confusing when describing how connections should be
    resumed.
    
    The voice gateway interface from the normal gateway is still incomplete.
    Probably will add some sort of pipe mechanism to communicate with voice
    gateways.

--- src/gateway.cc
+++ src/gateway.cc
@@ -1,26 +1,44 @@
-#include "gateway.h"
-#include <events/heartbeater.h>
+#include <events/event_listener.h>
 #include <events/voice_state_listener.h>
+#include <heartbeater.h>
+#include <iostream>
 
-cmd::discord::gateway::gateway(cmd::websocket::socket &sock, const std::string &token)
-    : sock{sock}, token{token}
+#include "gateway.h"
+
+cmd::discord::gateway::gateway(const std::string &token)
+    : websocket{}, beater{}, token{token}, state{connection_state::disconnected}
 {
-    // On hello opcode, spawns a thread and periodically sends a heartbeat message through *this
-    // gateway. On destruction it stops the heartbeat thread and joins it
-    auto beat = std::make_shared<heartbeater>(this);
-    private_handlers.emplace(gtw_op_recv::hello, beat);
-    private_handlers.emplace(gtw_op_recv::heartbeat_ack, beat);
+    gateway_event_map.emplace("READY", [&](nlohmann::json &data) -> void {
+        auto version = data["v"];
+        if (version.is_number()) {
+            if (version.get<int>() != 6)
+                throw std::runtime_error(
+                    "Unsupported gateway protocol version: " + std::to_string(version.get<int>()) +
+                    ". Support is limited to v6");
+        } else {
+            throw std::runtime_error("Expected version number in READY event");
+        }
 
-    nlohmann::json identify{
-        {"op", 2},
-        {"d",
-         {{"token", token},
-          {"properties",
-           {{"$os", "linux"}, {"$browser", "cmd-discord"}, {"$device", "cmd-discord"}}},
-          {"compress", compress},
-          {"large_threshold", large_threshold}}}};
-    std::string message = identify.dump(0);
-    sock.send(message);
+        // We got the READY event, we are now connected
+        state = connection_state::connected;
+
+        auto user_obj = data["user"];
+        if (user_obj.is_object()) {
+            auto id = user_obj["id"];
+            if (id.is_string()) {
+                user_id = id.get<std::string>();
+            }
+        }
+        auto session = data["session_id"];
+        if (session.is_string())
+            session_id = session.get<std::string>();
+    });
+
+    gateway_event_map.emplace(
+        "RESUME", [&](nlohmann::json &) -> void { state = connection_state::connected; });
+
+    // Establish the WebSocket connection
+    websocket.connect("wss://gateway.discord.gg/?v=6&encoding=json");
 }
 
 cmd::discord::gateway::~gateway() {}
@@ -28,12 +46,19 @@ cmd::discord::gateway::~gateway() {}
 void cmd::discord::gateway::next_event()
 {
     // Read the next message from the WebSocket and place it in buffer
-    sock.next_message(buffer);
+    auto read = websocket.next_message(buffer);
+    if (read == 0) {
+        throw std::runtime_error("WebSocket connection interrupted");
+    }
+    std::cout << "GATEWAY: ";
+    std::cout.write((char*) buffer.data(), read);
+    std::cout << "\n";
+
     // Parse the results as a json object
     auto json = nlohmann::json::parse(buffer.begin(), buffer.end());
 
     auto op = json["op"];
-    auto d = json["d"];
+    auto event_data = json["d"];
 
     if (op.is_number()) {
         int op_val = op.get<int>();
@@ -42,26 +67,39 @@ void cmd::discord::gateway::next_event()
                 seq_num = json["s"].get<int>();
             }
         }
-        std::string t_val = json["t"].is_string() ? json["t"].get<std::string>() : "";
+        std::string event_name = json["t"].is_string() ? json["t"].get<std::string>() : "";
 
         auto gateway_op = static_cast<gtw_op_recv>(op_val);
         switch (gateway_op) {
             case gtw_op_recv::dispatch:
-                // Run all public dispactch handlers
-                for (auto &handler : public_handlers) {
-                    handler.second->handle(gateway_op, d, t_val);
-                }
+                run_public_dispatch(gateway_op, event_data, event_name);
+                run_gateway_dispatch(event_data, event_name);
                 break;
             case gtw_op_recv::heartbeat:
                 heartbeat();  // Respond to heartbeats with a heartbeat
                 break;
             case gtw_op_recv::reconnect:
+                // We are asked to reconnect, i.e. we are disconnected
+                state = connection_state::disconnected;
+                resume();
                 break;
             case gtw_op_recv::invalid_session:
+                if (state == connection_state::disconnected) {
+                    throw std::runtime_error("Could not connect to Discord Gateway");
+                } else {
+                    state = connection_state::disconnected;
+                    // Already connected, if we can reconnect, try to resume the connection
+                    if (event_data.is_boolean() && event_data.get<bool>())
+                        resume();
+                    else
+                        throw std::runtime_error("");
+                }
                 break;
             case gtw_op_recv::hello:
+                beater.on_hello(*this, event_data);
                 break;
             case gtw_op_recv::heartbeat_ack:
+                beater.on_heartbeat_ack();
                 break;
             default:
                 throw std::runtime_error("Unknown opcode: " + std::to_string(op_val));
@@ -69,14 +107,23 @@ void cmd::discord::gateway::next_event()
     }
 }
 
-void cmd::discord::gateway::add_listener(const std::string &name, event_listener::ptr h)
+void cmd::discord::gateway::add_listener(const std::string &event_name,
+                                         const std::string &handler_name, event_listener::ptr h)
 {
-    public_handlers.emplace(name, h);
+    event_name_to_handler_name.emplace(event_name, handler_name);
+    handler_name_to_handler_ptr.emplace(handler_name, h);
 }
 
-void cmd::discord::gateway::remove_listener(const std::string &name)
+void cmd::discord::gateway::remove_listener(const std::string &event_name,
+                                            const std::string &handler_name)
 {
-    public_handlers.erase(name);
+    auto range = event_name_to_handler_name.equal_range(event_name);
+    for (auto it = range.first; it != range.second; ++it) {
+        if (it->second == handler_name) {
+            event_name_to_handler_name.erase(it);
+            handler_name_to_handler_ptr.erase(handler_name);
+        }
+    }
 }
 
 void cmd::discord::gateway::heartbeat()
@@ -97,24 +144,100 @@ void cmd::discord::gateway::safe_send(const std::string &s)
     } else {
         last_msg_sent = now;
     }
-    sock.send(s);
+    websocket.send(s);
 }
 
 void cmd::discord::gateway::join_voice_server(const std::string &guild_id,
                                               const std::string &channel_id)
 {
+    std::string name = user_id + guild_id + channel_id + "voice_state_listener";
+    auto handler = std::make_shared<voice_state_listener>(user_id, channel_id, guild_id);
+    add_listener("VOICE_STATE_UPDATE", name, handler);
+    add_listener("VOICE_SERVER_UPDATE", name, handler);
+
     nlohmann::json json{{"op", static_cast<int>(gtw_op_send::voice_state_update)},
                         {"d",
                          {{"guild_id", guild_id},
                           {"channel_id", channel_id},
                           {"self_mute", false},
                           {"self_deaf", false}}}};
-    std::string name = user_id + guild_id + channel_id + "voice_state_listener";
-    add_listener(name, std::make_shared<voice_state_listener>(this, user_id, guild_id, channel_id));
-    safe_send(json.dump(0));
+    safe_send(json.dump());
 }
 
 void cmd::discord::gateway::leave_voice_server(const std::string &guild_id,
                                                const std::string &channel_id)
 {
+    std::string name = user_id + guild_id + channel_id + "voice_state_listener";
+    remove_listener("VOICE_STATE_UPDATE", name);
+    remove_listener("VOICE_SERVER_UPDATE", name);
+}
+
+void cmd::discord::gateway::identify()
+{
+    nlohmann::json identify_payload{
+        {"op", static_cast<int>(gtw_op_send::identify)},
+        {"d",
+         {{"token", token},
+          {"properties",
+           {{"$os", "linux"}, {"$browser", "cmd-discord"}, {"$device", "cmd-discord"}}},
+          {"compress", compress},
+          {"large_threshold", large_threshold}}}};
+    std::string message = identify_payload.dump();
+    safe_send(message);
+}
+
+void cmd::discord::gateway::resume()
+{
+    // If we are connected, ignore any resumes
+    if (state == connection_state::connected)
+        return;
+
+    if (session_id.empty())
+        throw std::runtime_error("Could not resume previous session: no such session");
+
+    // Close the previous connection and create a new websocket
+    websocket.close(cmd::websocket_status_code::going_away);
+    websocket = cmd::websocket{};
+    websocket.connect("wss://gateway.discord.gg/?v=6&encoding=json");
+
+    nlohmann::json resume_payload{
+        {"op", static_cast<int>(gtw_op_send::resume)},
+        {"d", {{"token", token}, {"session_id", session_id}, {"seq", seq_num}}}};
+    safe_send(resume_payload.dump());
+}
+
+std::string cmd::discord::gateway::get_user_id()
+{
+    return user_id;
+}
+
+std::string cmd::discord::gateway::get_session_id()
+{
+    return session_id;
+}
+
+void cmd::discord::gateway::run_public_dispatch(gtw_op_recv op, nlohmann::json &data,
+                                                const std::string &t)
+{
+    auto range = event_name_to_handler_name.equal_range(t);
+    for (auto it = range.first; it != range.second; ++it) {
+        auto handler = handler_name_to_handler_ptr.find(it->second);
+        if (handler != handler_name_to_handler_ptr.end())
+            handler->second->handle(*this, op, data, t);
+    }
+    // Run all event listeners subscribing to 'ALL'
+    auto range_all = event_name_to_handler_name.equal_range("ALL");
+    for (auto it = range_all.first; it != range_all.second; ++it) {
+        auto handler = handler_name_to_handler_ptr.find(it->second);
+        if (handler != handler_name_to_handler_ptr.end())
+            handler->second->handle(*this, op, data, t);
+    }
+}
+
+void cmd::discord::gateway::run_gateway_dispatch(nlohmann::json &data,
+                                                 const std::string &event_name)
+{
+    auto event_it = gateway_event_map.find(event_name);
+    if (event_it != gateway_event_map.end())
+        event_it->second(data);
 }
--- src/gateway.h
+++ src/gateway.h
@@ -8,44 +8,63 @@
 
 #include <api.h>
 #include <events/event_listener.h>
+#include <heartbeater.h>
+#include <voice_gateway.h>
 #include <json.hpp>
 
 namespace cmd
 {
 namespace discord
 {
-class gateway
+class gateway : public beatable
 {
 public:
-    explicit gateway(cmd::websocket::socket &sock, const std::string &token);
+    explicit gateway(const std::string &token);
     ~gateway();
-    void add_listener(const std::string &name, event_listener::ptr h);
-    void remove_listener(const std::string &name);
+    void add_listener(const std::string &event_name, const std::string &handler_name,
+                      event_listener::ptr h);
+    void remove_listener(const std::string &event_name, const std::string &handler_name);
     void next_event();
-    void heartbeat();
+    void heartbeat() override;
+    void identify();
+    void resume();
 
     void join_voice_server(const std::string &guild_id, const std::string &channel_id);
     void leave_voice_server(const std::string &guild_id, const std::string &channel_id);
 
+    std::string get_user_id();
+    std::string get_session_id();
+
+    void add_voice_gateway(const std::string &channel_id);
+
 private:
     using clock = std::chrono::steady_clock;
     void safe_send(const std::string &s);
+    void run_public_dispatch(gtw_op_recv op, nlohmann::json &data, const std::string &t);
+    void run_gateway_dispatch(nlohmann::json &data, const std::string &event_name);
 
-    cmd::websocket::socket sock;
+    cmd::websocket websocket;
     std::vector<unsigned char> buffer;
 
-    std::map<std::string, event_listener::ptr> public_handlers;
-    std::multimap<gtw_op_recv, event_listener::ptr> private_handlers;
+    // Map an event name (e.g. READY, RESUMED, etc.) to a handler name
+    std::multimap<std::string, std::string> event_name_to_handler_name;
+    // Map a handler name to a event_lister
+    std::map<std::string, event_listener::ptr> handler_name_to_handler_ptr;
+
+    std::map<std::string, std::function<void(nlohmann::json &)>> gateway_event_map;
+
+    heartbeater beater;
 
     std::string token;
     std::mutex write_mutex;
 
     clock::time_point last_msg_sent;
 
-    std::string user_id;
+    std::string user_id, session_id;
     int seq_num;
     const bool compress = false;
     const int large_threshold = 250;
+    enum class connection_state { disconnected, connected } state;
 };
 }
 }
