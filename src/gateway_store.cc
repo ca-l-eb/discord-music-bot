8c9f1bf1951085d53115efc5e61d46aaa8ada0c0
Caleb Dechant <calebdechant@gmail.com>
2017-10-29 19:07:54 -0700

    Update gateway_store, now can get guild object

--- src/gateway_store.cc
+++ src/gateway_store.cc
@@ -11,10 +11,18 @@ void cmd::discord::gateway_store::parse_guild(const nlohmann::json &json) {
         for (auto &member: g.members)
             user_to_guilds.insert(std::pair<std::string, std::string>(member.user.id, g.id));
         
-        guilds.insert(std::move(g));
+        guilds[g.id] = std::move(g);
     } catch (std::exception &e) {
         // Do nothing if we get an exception
     }
+}
+
+cmd::discord::guild cmd::discord::gateway_store::get_guild(const std::string &guild_id)
+{
+    auto it = guilds.find(guild_id);
+    if (it != guilds.end())
+        return it->second;
+    return cmd::discord::guild{};
 }
 
 const std::string cmd::discord::gateway_store::lookup_channel(const std::string &channel_id)
--- src/gateway_store.h
+++ src/gateway_store.h
@@ -18,9 +18,10 @@ public:
     
     // Returns the guild_id that the channel is in
     const std::string lookup_channel(const std::string &channel_id);
+    cmd::discord::guild get_guild(const std::string &guild_id);
 
 private:
-    std::set<guild> guilds;
+    std::map<std::string, guild> guilds;
     std::map<std::string, std::string> channels_to_guild;
     std::multimap<std::string, std::string> user_to_guilds;
 };
